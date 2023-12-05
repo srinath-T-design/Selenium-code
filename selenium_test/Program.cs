0748d15b7ead0b87b2a33059ecd680c517b21265
srinath-T-design <srinaththiyagarajan707@gmail.com>
2023-12-05 16:22:39 +0530

    selenium code commit

---
+++ selenium_test/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace selenium_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test case started ");
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(2000);
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            ele.SendKeys("javatpoint tutorials");
+            Thread.Sleep(2000);
+            IWebElement ele1 = driver.FindElement(By.Name("btnI"));
+            ele1.Click();
+            Thread.Sleep(3000);
+            driver.Close();
+            Console.Write("test case ended ");
+        }
+    }
+}
